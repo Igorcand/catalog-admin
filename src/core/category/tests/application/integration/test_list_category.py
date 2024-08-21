cb1a6bc71ef4fd2cb0035897ffaeff9b70fc567a
Igor Candido <igorcr39@gmail.com>
2024-08-21 13:49:56 -0300

    feat: List categories use cases + tests

---
+++ src/core/category/tests/application/integration/test_list_category.py
@@ -0,0 +1,46 @@
+from unittest.mock import create_autospec
+from uuid import uuid4
+from src.core.category.application.use_cases.list_category import ListCategory, ListCategoryRequest, ListCategoryResponse, CategoryOutput
+from src.core.category.application.use_cases.exceptions import CategoryNotFound
+from src.core.category.application.category_repository import CategoryRepository
+from src.core.category.domain.category import Category
+import pytest
+from src.core.category.infra.in_memory_category_repository import InMemoryCategoryRepository
+
+
+
+class TestListCategory:
+    def test_then_return_empty_list(self):
+        category = Category(
+            name="Filme", 
+            description="Desc", 
+        )
+        repository = InMemoryCategoryRepository()
+
+        use_case = ListCategory(repository=repository)
+        request = ListCategoryRequest()
+        response = use_case.execute(request)
+
+        assert response == ListCategoryResponse(data=[])
+    
+    def test__return_existing_categories(self):
+        category = Category(
+            name="Filme", 
+            description="Desc", 
+        )
+        repository = InMemoryCategoryRepository(categories=[category])
+
+        use_case = ListCategory(repository=repository)
+        request = ListCategoryRequest()
+        response = use_case.execute(request)
+
+        assert response == ListCategoryResponse(
+            data=[
+                CategoryOutput(
+                    id=category.id,
+                    name=category.name,
+                    description=category.description,
+                    is_active=category.is_active,
+                )
+            ]
+        )
---
+++ src/core/category/tests/application/unit/test_list_category.py
@@ -0,0 +1,47 @@
+from unittest.mock import create_autospec
+from uuid import uuid4
+from src.core.category.application.use_cases.list_category import ListCategory, ListCategoryRequest, ListCategoryResponse, CategoryOutput
+from src.core.category.application.use_cases.exceptions import CategoryNotFound
+from src.core.category.application.category_repository import CategoryRepository
+from src.core.category.domain.category import Category
+import pytest
+
+
+
+class TestListCategory:
+    def test_when_no_categories_in_repository_then_return_empty_list(self):
+        category = Category(
+            name="Filme", 
+            description="Desc", 
+        )
+        mock_repository = create_autospec(CategoryRepository)
+        mock_repository.list.return_value = []
+
+        use_case = ListCategory(repository=mock_repository)
+        request = ListCategoryRequest()
+        response = use_case.execute(request)
+
+        assert response == ListCategoryResponse(data=[])
+    
+    def test_when_categories_in_repository_then_return_list(self):
+        category = Category(
+            name="Filme", 
+            description="Desc", 
+        )
+        mock_repository = create_autospec(CategoryRepository)
+        mock_repository.list.return_value = [category]
+
+        use_case = ListCategory(repository=mock_repository)
+        request = ListCategoryRequest()
+        response = use_case.execute(request)
+
+        assert response == ListCategoryResponse(
+            data=[
+                CategoryOutput(
+                    id=category.id,
+                    name=category.name,
+                    description=category.description,
+                    is_active=category.is_active,
+                )
+            ]
+        )
