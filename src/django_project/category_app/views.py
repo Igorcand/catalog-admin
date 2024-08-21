b13c26e24516e802cb1fd447070ce42f124c8ee3
Igor Candido <igorcr39@gmail.com>
2024-08-21 14:30:06 -0300

    feat: Create API to list static categories

--- src/django_project/category_app/views.py
+++ src/django_project/category_app/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.request import Request
+from rest_framework.status import HTTP_200_OK
 
-# Create your views here.
+
+
+class CategoryViewSet(viewsets.ViewSet):
+    def list(self, request: Request) -> Response:
+        return Response(status=HTTP_200_OK, data=[
+            {
+                "id": "235387631586",
+                "name": "Movie",
+                "description": "Description",
+                "is_active": True
+            }
+        ])
