4eaaffc061a3ea677965afc289499e01e8f73a78
Igor Candido <igorcr39@gmail.com>
2024-08-21 14:44:46 -0300

    test: Api test

--- src/django_project/category_app/tests.py
+++ src/django_project/category_app/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+class TestCategoryAPI(APITestCase):
+    def test_list_categories(self):
+        url = "/api/categories/"
+        response = self.client.get(url)
+
+        expected_data = [
+            {
+                "id": "235387631586",
+                "name": "Movie",
+                "description": "Description",
+                "is_active": True
+            }
+        ]
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, expected_data)
+
+        
