1ae2fa0ee85c28d8321583afce0e7d6620be9279
Igor Candido <igorcr39@gmail.com>
2024-08-22 06:44:00 -0300

    feat: Implement list category route with access to db

--- src/django_project/category_app/tests/test_category_api.py
+++ src/django_project/category_app/tests/test_category_api.py
@@ -1,17 +1,26 @@
 from django.test import TestCase
 from rest_framework.test import APITestCase
+from django_project.category_app.repository import DjangoORMCategoryRepository
+from src.core.category.domain.category import Category
 
 class TestCategoryAPI(APITestCase):
     def test_list_categories(self):
+        category = Category(
+            name= "Movie",
+            description="Description"
+        )
+        repository = DjangoORMCategoryRepository()
+        repository.save(category)
+
         url = "/api/categories/"
         response = self.client.get(url)
 
         expected_data = [
             {
-                "id": "235387631586",
-                "name": "Movie",
-                "description": "Description",
-                "is_active": True
+                "id": str(category.id),
+                "name": category.name,
+                "description": category.description,
+                "is_active": category.is_active,
             }
         ]
         self.assertEqual(response.status_code, 200)
--- src/django_project/category_app/views.py
+++ src/django_project/category_app/views.py
@@ -4,15 +4,24 @@ from rest_framework.response import Response
 from rest_framework.request import Request
 from rest_framework.status import HTTP_200_OK
 
+from src.core.category.application.use_cases.list_category import ListCategoryRequest, ListCategory, ListCategoryResponse
+from django_project.category_app.repository import DjangoORMCategoryRepository
 
 
 class CategoryViewSet(viewsets.ViewSet):
     def list(self, request: Request) -> Response:
-        return Response(status=HTTP_200_OK, data=[
+        input = ListCategoryRequest()
+        use_case = ListCategory(repository=DjangoORMCategoryRepository())
+        output = use_case.execute(input)
+
+        categories = [
             {
-                "id": "235387631586",
-                "name": "Movie",
-                "description": "Description",
-                "is_active": True
-            }
-        ])
+                "id": str(category.id),
+                "name": category.name,
+                "description": category.description,
+                "is_active": category.is_active,
+            } for category in output.data
+        ]
+
+
+        return Response(status=HTTP_200_OK, data=categories)
