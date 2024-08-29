524ddb7dc4058197b53f1b599e033c029d18efc3
Igor Candido <igorcr39@gmail.com>
2024-08-29 06:00:19 -0300

    feat: Genre use case list genre + integration and unit tests

---
+++ src/core/genre/application/use_cases/list_genre.py
@@ -0,0 +1,37 @@
+from uuid import UUID
+from dataclasses import dataclass, field
+from src.core.genre.application.exceptions import RelatedCategoriesNotFound, InvalidGenre
+from src.core.genre.domain.genre import Genre
+from src.core.genre.domain.genre_repository import GenreRepository
+
+@dataclass
+class GenreOutput:
+    id: UUID
+    name: str
+    is_active: bool
+    categories: set[UUID]
+
+class ListGenre:
+    def __init__(self, repository: GenreRepository) -> None:
+        self.repository = repository
+
+    @dataclass
+    class Input:
+        pass
+
+    @dataclass
+    class Output:
+        data: list[GenreOutput]
+
+
+    def execute(self, input: Input):
+        genres = self.repository.list()
+        mapped_genres = [
+            GenreOutput(
+                id=genre.id,
+                name=genre.name,
+                is_active=genre.is_active,
+                categories=genre.categories,
+            ) for genre in genres
+        ]
+        return self.Output(data=mapped_genres)
---
+++ src/core/genre/tests/application/integration/test_list_genre.py
@@ -0,0 +1,59 @@
+import pytest 
+from uuid import uuid4, UUID
+from src.core.category.domain.category import Category
+from src.core.genre.application.use_cases.create_genre import CreateGenre
+from src.core.genre.domain.genre_repository import GenreRepository
+from src.core.category.domain.category_repository import CategoryRepository
+from src.core.category.infra.in_memory_category_repository import InMemoryCategoryRepository
+from src.core.genre.infra.in_memory_genre_repository import InMemoryGenreRepository
+
+from src.core.genre.application.exceptions import RelatedCategoriesNotFound, InvalidGenre
+from src.core.genre.domain.genre import Genre
+from src.core.genre.application.use_cases.list_genre import ListGenre, GenreOutput
+
+
+class TestListGenre:
+    def test_list_genres_with_associated_categories(self):
+        category_repository = InMemoryCategoryRepository()
+        movie_category = Category(name="Movie")
+        documentary_category = Category(name="Documentary")
+
+        category_repository.save(movie_category)
+        category_repository.save(documentary_category)
+
+        genre_repository = InMemoryGenreRepository()
+        genre = Genre(name="Drama", categories={movie_category.id,documentary_category.id})
+        genre_repository.save(genre)
+
+        use_case = ListGenre(repository=genre_repository)
+        output = use_case.execute(input=ListGenre.Input())
+
+        assert len(output.data) == 1
+        assert output == ListGenre.Output(
+            data = [GenreOutput(
+                    id = genre.id,
+                    name=genre.name,
+                    is_active=True,
+                    categories={movie_category.id, documentary_category.id}
+                )
+            ]
+        )
+    
+    def test_list_genres_without_associated_categories(self):
+        genre_repository = InMemoryGenreRepository()
+        genre = Genre(name="Drama", categories=set())
+        genre_repository.save(genre)
+
+        use_case = ListGenre(repository=genre_repository)
+        output = use_case.execute(input=ListGenre.Input())
+
+        assert len(output.data) == 1
+        assert output == ListGenre.Output(
+            data = [GenreOutput(
+                    id = genre.id,
+                    name=genre.name,
+                    is_active=True,
+                    categories=set()
+                )
+            ]
+        )
