8020467f5c0c54f881370231937bceee6cb3d7db
Igor Candido <igorcr39@gmail.com>
2024-09-02 05:35:02 -0300

    feat: Create cast member use cases

---
+++ src/core/cast_member/application/use_cases/create_cast_member.py
@@ -0,0 +1,33 @@
+from uuid import UUID
+from dataclasses import dataclass
+from src.core.cast_member.application.use_cases.exceptions import InvalidCastMember
+from src.core.cast_member.domain.cast_member_repository import CastMemberRepository
+from src.core.cast_member.domain.cast_member import CastMember, CastMemberType
+
+@dataclass
+class CreateCastMemberRequest:
+    name: str
+    type: CastMemberType
+
+
+@dataclass
+class CreateCastMemberResponse:
+    id: UUID
+
+
+class CreateCastMember:
+    def __init__(self, repository: CastMemberRepository) -> None:
+        self.repository = repository
+
+
+    def execute(self, request: CreateCastMemberRequest):
+        try:
+            cast_member = CastMember(
+                name=request.name,
+                type=request.type,
+            )
+        except ValueError as e:
+            raise InvalidCastMember(e)
+        
+        self.repository.save(cast_member)
+        return CreateCastMemberResponse(id=cast_member.id)
