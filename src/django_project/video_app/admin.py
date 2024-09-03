e871340bcf459ce75035e11dc6f69dd3512f68a5
Igor Candido <igorcr39@gmail.com>
2024-09-03 20:27:32 -0300

    feat: Config admin

--- src/django_project/video_app/admin.py
+++ src/django_project/video_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from src.django_project.video_app.models import Video
 
-# Register your models here.
+class VideoAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Video, VideoAdmin)
