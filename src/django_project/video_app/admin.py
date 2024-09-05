0c55e6c273b86528b58ea22b54e3415119551be3
Igor Candido <igorcr39@gmail.com>
2024-09-04 21:08:17 -0300

    registred AudioVideoMedia in admin

--- src/django_project/video_app/admin.py
+++ src/django_project/video_app/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from src.django_project.video_app.models import Video
+from src.django_project.video_app.models import Video, AudioVideoMedia
 
 class VideoAdmin(admin.ModelAdmin):
     pass
 
+class AudioVideoMediaAdmin(admin.ModelAdmin):
+    pass
+
 admin.site.register(Video, VideoAdmin)
+admin.site.register(AudioVideoMedia, AudioVideoMediaAdmin)
+
