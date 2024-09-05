4f68c55e9dc0c2274ccc037a8c2df1bc19bcf736
Igor Candido <igorcr39@gmail.com>
2024-09-05 06:26:58 -0300

    test: Create unit and integration tests for list video use case

---
+++ src/core/video/tests/application/integration/test_list_video.py
@@ -0,0 +1,79 @@
+from decimal import Decimal
+from uuid import uuid4
+from src.core.video.domain.value_objects import Rating
+from src.core.video.application.use_cases.list_videos import ListOutput, ListVideo, VideoOutput
+from src.core.video.infra.in_memory_video_repository import InMemoryVideoRepository
+from src.core.video.domain.video import Video
+from unittest.mock import create_autospec
+from src.core._shered.domain.pagination import ListOutputMeta
+import pytest
+
+@pytest.mark.video
+class TestListVidewo:
+    def test_list_video_should_success(self):
+        
+        genre_repository = InMemoryVideoRepository()
+
+        video_1 = Video(
+            title="Sample Video1",
+            description="A test video",
+            launch_year=2022,
+            duration=Decimal("120.5"),
+            opened=False,
+            rating=Rating.AGE_14,
+            categories=set(),
+            genres=set(),
+            cast_members=set(),
+        )
+
+        video_2 = Video(
+            title="Sample Video2",
+            description="A test video description",
+            launch_year=2022,
+            duration=Decimal("120.5"),
+            opened=False,
+            rating=Rating.AGE_12,
+            categories=set(),
+            genres=set(),
+            cast_members=set(),
+        )
+
+        genre_repository.save(video_1)
+        genre_repository.save(video_2)
+
+
+        use_case = ListVideo(repository=genre_repository)
+        output = use_case.execute(ListVideo.Input())
+
+        assert output == ListVideo.Output(
+            data=[
+                VideoOutput(
+                    id=video_1.id,
+                    title="Sample Video1",
+                    description="A test video",
+                    launch_year=2022,
+                    duration=Decimal("120.5"),
+                    opened=False,
+                    published=False,
+                    rating=Rating.AGE_14,
+                    categories=set(),
+                    genres=set(),
+                    cast_members=set()
+                ),
+                VideoOutput(
+                    id=video_2.id,
+                    title="Sample Video2",
+                    description="A test video description",
+                    launch_year=2022,
+                    duration=Decimal("120.5"),
+                    opened=False,
+                    published=False,
+                    rating=Rating.AGE_12,
+                    categories=set(),
+                    genres=set(),
+                    cast_members=set()
+                ),
+            ],
+            meta = ListOutputMeta(current_page=1, per_page=2, total=2)
+        )
+
