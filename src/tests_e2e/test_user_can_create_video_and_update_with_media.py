15090027751515abdd7801ca16398594329c79e0
Igor Candido <igorcr39@gmail.com>
2024-09-05 15:20:31 -0300

    test: Create e2e test with upload video media

---
+++ src/tests_e2e/test_user_can_create_video_and_update_with_media.py
@@ -0,0 +1,37 @@
+import pytest 
+from rest_framework.test import APIClient
+from django.core.files.uploadedfile import SimpleUploadedFile
+
+@pytest.mark.django_db
+@pytest.mark.e2e
+class TestCreateVIdeoWithoutMediaAndUpdate:
+    def test_user_can_video_without_media_and_with_no_related_entities_and_update_with_media(self) -> None:
+        api_client = APIClient()
+
+        #create item
+        create_response = api_client.post(
+            "/api/videos/",
+            data={
+                "title": "A samble video",
+                "description": "video description",
+                "launch_year": 2024,
+                "duration": 120.5,
+                "rating": "AGE_12",
+                "categories": [],
+                "genres": [],
+                "cast_members": []
+            }, 
+            format="json"
+            )
+
+        assert create_response.status_code == 201
+        created_video_id = create_response.data["id"]
+
+        url = f"/api/videos/{created_video_id}/"
+
+        mp4_file = SimpleUploadedFile("test.mp4", b"fake_mp4_content", content_type="video/mp4")
+
+        data = {"video_file": mp4_file}
+        response = api_client.patch(url, data=data, format="multipart")
+
+        assert response.status_code == 200
