bcd6d591a2be9437f6d02ef748341a74ff87cbb6
Igor Candido <igorcr39@gmail.com>
2024-09-09 10:53:04 -0300

    feat: register event and handler in message bus

--- src/core/_shered/events/message_bus.py
+++ src/core/_shered/events/message_bus.py
@@ -1,16 +1,30 @@
 from typing import Type
 from src.core._shered.application.handler import Handler
 from src.core._shered.events.abstract_message_bus import AbstractMessageBus
+from src.core._shered.infrastructure.events.rabbitmq_dispatcher import RabbitMQDispatcher
+
+from src.core.video.application.events.integration_events import AudioVideoMediaUpdatedIntegrationEvent
+from src.core.video.application.events.handlers import PublishAudioVideoMediaUpdatedHandler
+
 from src.core._shered.events.event import Event
+from src.core._shered.events.event_dispatcher import Event
+
 
 class MessageBus(AbstractMessageBus):
     def __init__(self) -> None:
-        self.handlers: dict[Type[Event], list[Handler]] = {}
+        self.handlers: dict[Type[Event], list[Handler]] = {
+            AudioVideoMediaUpdatedIntegrationEvent: [
+                PublishAudioVideoMediaUpdatedHandler(
+                    event_dispatcher=RabbitMQDispatcher(queue="videos.new")
+                )
+            ],
+        }
 
     def handle(self, events: list[Event]) -> None:
         print(f"Handling events: {events}")
         for event in events:
             handlers = self.handlers.get(type(event), [])
+            print(f"handlers by type {type(event)}: {handlers}")
             for handler in handlers:
                 try:
                     handler.handle(event)
