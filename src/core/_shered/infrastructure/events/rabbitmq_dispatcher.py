f688a0faa78ca3844968fdc7b2109e088e3b9ed2
Igor Candido <igorcr39@gmail.com>
2024-09-10 05:51:23 -0300

    feat: Config rabbitmq queue dispatcher

--- src/core/_shered/infrastructure/events/rabbitmq_dispatcher.py
+++ src/core/_shered/infrastructure/events/rabbitmq_dispatcher.py
@@ -1,9 +1,33 @@
 from src.core._shered.events.event import Event
 from src.core._shered.events.event_dispatcher import EventDispatcher
+import pika
+import json
+from dataclasses import asdict
 
 class RabbitMQDispatcher(EventDispatcher):
-    def __init__(self, queue="videos.new") -> None:
+    """
+    docker run -d --hostname rabbitmq --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:3-management
+    """
+    def __init__(self, host='localhost', queue="videos.new") -> None:
         self.queue = queue
+        self.host = host
+        self.connection = None
+        self.channel = None
+
         
     def dispatch(self, event: Event) -> None:
-        print(f'RabbitMQ Dispatching event = {event}')
+        if not self.connection:
+            self.connection = pika.BlockingConnection(pika.ConnectionParameters(host=self.host))
+            self.channel = self.connection.channel()
+            self.channel.queue_declare(queue=self.queue)
+        
+        self.channel.basic_publish(
+            exchange='',
+            routing_key=self.queue,
+            body=json.dumps(asdict(event))
+        )
+
+        print(f'RabbitMQ Dispatched event = {event}')
+
+    def close(self):
+        self.connection.close()
