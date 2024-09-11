f1060ccca80972fa80a4d50a886b73eb5cb5c5f6
Igor Candido <igorcr39@gmail.com>
2024-09-11 14:08:43 -0300

    feat: Create permissions classes to consume auth interface

---
+++ src/django_project/permissions.py
@@ -0,0 +1,16 @@
+from rest_framework.permissions import BasePermission
+from src.core._shered.infrastructure.auth.jwt_auth_service import JwtAuthService
+
+class IsAuthenticated(BasePermission):
+    def has_permission(self, request, view):
+        token = request.headers.get("Authentication", "")
+        if not JwtAuthService(token).is_authenticated():
+            return False
+        return True
+
+class IsAdmin(BasePermission):
+    def has_permission(self, request, view):
+        token = request.headers.get("Authentication", "")
+        if not JwtAuthService(token).has_role("admin"):
+            return False
+        return True
